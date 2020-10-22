1cf837d85568468ba7cb8ec8f5170d6fc4ad53b9
Renju jose <renju.jose@testsigma.com>
2020-10-22 22:53:13 +0530

    Update trigger_ts_run.sh
    - Cleanup response
    - Helper function to convert runtime seconds to minutes

--- trigger_ts_run.sh
+++ trigger_ts_run.sh
@@ -1,41 +1,46 @@
 #!/bin/bash
-
+#********START USER_INPUTS*********
 TESTSIGMA_USER_NAME=renju.jose@testsigma.com
 TESTSIGMA_PASSWORD=testsigma123
 TESTSIGMA_TEST_PLAN_RUN_URL=https://app.testsigma.com/rest/execution/2090/run
-MAX_WAIT_TIME_FOR_SCRIPT_TO_EXIT=180
+MAX_WAIT_TIME_FOR_SCRIPT_TO_EXIT=60
 #********END USER_INPUTS***********
+##########GLOBAL variables####################################################
+POLL_INTERVAL_FOR_RUN_STATUS=1
+NO_OF_POLLS=$((MAX_WAIT_TIME_FOR_SCRIPT_TO_EXIT/POLL_INTERVAL_FOR_RUN_STATUS))
+SLEEP_TIME=$((POLL_INTERVAL_FOR_RUN_STATUS * 10))
+JSON_FILE_NAME=testsigma.json
+REPORT_FILE_NAME=$JSON_OUTPUT_REPORTS_DIR/$JSON_FILE_NAME
+LOG_CONTENT=""
+##############################################################################
 
-get_status(){
+#####################FUNCTIONS########################
+function get_status(){
     RUN_RESPONSE=$(curl -u $TESTSIGMA_USER_NAME:$TESTSIGMA_PASSWORD --silent --write-out "HTTPSTATUS:%{http_code}" -X GET $TESTSIGMA_TEST_PLAN_RUN_URL/$HTTP_BODY/status)
-    echo "Status_Response_Complete: $RUN_RESPONSE "
     # extract the body
     RUN_BODY=$(echo $RUN_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
-    # extract the status
+    # extract the response status
     RUN_STATUS=$(echo $RUN_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
-    echo "Response_Status: $RUN_STATUS"
-    # print the body
-    echo "Run Status..."
-    echo "$RUN_BODY"
+    # extract exec status
     EXECUTION_STATUS=$(echo $RUN_BODY | getJsonValue status)
-
-    echo "Execution Status: $EXECUTION_STATUS"
 }
 
 function checkTestPlanRunStatus(){
   IS_TEST_RUN_COMPLETED=0
-  for((i=0; i<= NO_OF_POLLS;i++))
+  for ((i=0;i<=NO_OF_POLLS;i++))
   do
     get_status
-    if [ $EXECUTION_STATUS -eq 2 ]
-     then
-      echo "Sleep/Wait for $SLEEP_TIME seconds before next poll....."
+    if [ $EXECUTION_STATUS -eq 2 ]; then
+      echo "Poll #$(($i+1)) - Test Execution in progress... Wait for $SLEEP_TIME seconds before next poll.."
       sleep $SLEEP_TIME
-
-    else
+    elif [ $EXECUTION_STATUS -eq 0 ]; then
       IS_TEST_RUN_COMPLETED=1
-      echo "Automated Tests Execution completed...\n total script execution time:$(((i+1)*SLEEP_TIME/60)) minutes"
+      echo "Poll #$(($i+1)) - Tests Execution completed..."
+      TOTALRUNSECONDS=$(($(($i+1))*$SLEEP_TIME))
+      echo "Total script run time: $(convertsecs $TOTALRUNSECONDS)"
       break
+    else
+      echo "Unexpected Execution status. Please check run results for more details."
     fi
   done
 }
@@ -58,39 +63,42 @@ json_key=$1
 awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/\042'$json_key'\042/){print $(i+1)}}}' | tr -d '"'
 }
 
+function convertsecs(){
+  ((h=${1}/3600))
+  ((m=(${1}%3600)/60))
+  ((s=${1}%60))
+  printf "%02d hours %02d minutes %02d seconds" $h $m $s
+}
+######################################################
+
 echo "************Testsigma:Start executing automated tests ... ************"
 
-##########GLOBAL variables####################################################
-POLL_INTERVAL_FOR_RUN_STATUS=3
-NO_OF_POLLS=$((MAX_WAIT_TIME_FOR_SCRIPT_TO_EXIT/POLL_INTERVAL_FOR_RUN_STATUS))
-SLEEP_TIME=$((POLL_INTERVAL_FOR_RUN_STATUS * 60))
-JSON_FILE_NAME=testsigma.json
-REPORT_FILE_NAME=$JSON_OUTPUT_REPORTS_DIR/$JSON_FILE_NAME
-LOG_CONTENT=""
-##############################################################################
-echo "NO of polls $NO_OF_POLLS"
-# store the whole response with the status at the and
+# store the whole response with the status at the end
 HTTP_RESPONSE=$(curl -u $TESTSIGMA_USER_NAME:$TESTSIGMA_PASSWORD --silent --write-out "HTTPSTATUS:%{http_code}" -X POST $TESTSIGMA_TEST_PLAN_RUN_URL)
+
 # extract the body
-echo "HTTP_RESPONSE=$HTTP_RESPONSE \n"
 HTTP_BODY=$(echo $HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
+
 # extract the status
 HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
 
 # print the body
-echo "Run_ID:$HTTP_BODY"
+NUMBERS_REGEX="^[0-9].*"
+if [[ $HTTP_BODY =~ $NUMBERS_REGEX ]]; then
+  echo "Run ID: $HTTP_BODY"
+else
+  echo "$HTTP_BODY"
+fi
 
 # example using the status
 if [ ! $HTTP_STATUS -eq 200  ]; then
-
-  echo "Failed to executed automated tests!!"
-  echo "Error [HTTP status: $HTTP_STATUS]"
+  echo "Failed to start Test Plan execution! Status Code: $HTTP_STATUS"
   exit 1 #Exit with a failure.
 else
+  echo "Number of maximum polls to be done: $NO_OF_POLLS"
   checkTestPlanRunStatus
-  saveFinalResponseToAFile
-
 fi
 
-echo "Final response: $RUN_BODY"
+echo "************************************************"
+echo "Result JSON Response: $RUN_BODY"
 echo "************Testsigma:Completed executing automated tests ... ************"
