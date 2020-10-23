9fd492df594b38b847deeca7ad519abbf0a9d84e
Renju jose <renju.jose@testsigma.com>
2020-10-24 03:14:27 +0530

    Update trigger_ts_run.sh
    - Sleep time calculation simplified
    - Messages correction

--- trigger_ts_run.sh
+++ trigger_ts_run.sh
@@ -6,7 +6,7 @@
 # TESTSIGMA_TEST_PLAN_ID -> Testsigma Testplan ID.
 # You can get this from Testsigma App >> Test Plans >> <TEST_PLAN_NAME> >> CI/CD Integration
 #
-# MAX_WAIT_TIME_FOR_SCRIPT_TO_EXIT -> Maximum time the script will wait for TEST Plan execution to complete. 
+# MAX_WAIT_TIME_FOR_SCRIPT_TO_EXIT -> Maximum time in minutes the script will wait for TEST Plan execution to complete. 
 # The sctript will exit if the Maximum time is exceeded. However, the Test Plan will continue to run. 
 # You can check test results by logging to Testsigma.
 #
@@ -16,22 +16,20 @@
 #********START USER_INPUTS*********
 TESTSIGMA_API_KEY=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxYWQ3NjUxYy0zNzkwLTQ3ZWMtOTkxNC0xMjdlMDM0MTEyM2QiLCJkb21haW4iOiJ0ZXN0c2lnbWEuY29tIn0.5PsU_F4jTipqncDc0MyZbEA3mKflqkrvRGe06A6BbdaOwEz1wHExGR_mmn41FBjQygXVgLGv6RqZIVLTNpgv0w
 TESTSIGMA_TEST_PLAN_ID=2090
-MAX_WAIT_TIME_FOR_SCRIPT_TO_EXIT=60
+MAX_WAIT_TIME_FOR_SCRIPT_TO_EXIT=1
 JUNIT_REPORT_FILE_PATH=./junit-report-$(date +"%Y%m%d%H%M").xml
 RUNTIME_DATA_INPUT="url=https://the-internet.herokuapp.com/login,test=1221"
 BUILD_NO=$(date +"%Y%m%d%H%M")
 #********END USER_INPUTS***********
 
 #********GLOBAL variables**********
-POLL_INTERVAL_FOR_RUN_STATUS=1
-NO_OF_POLLS=$((MAX_WAIT_TIME_FOR_SCRIPT_TO_EXIT/POLL_INTERVAL_FOR_RUN_STATUS))
-SLEEP_TIME=$((POLL_INTERVAL_FOR_RUN_STATUS * 10))
+POLL_COUNT=5
+SLEEP_TIME=$(((MAX_WAIT_TIME_FOR_SCRIPT_TO_EXIT*60)/$POLL_COUNT))
 JSON_REPORT_FILE_PATH=./testsigma.json
 TESTSIGMA_TEST_PLAN_REST_URL=https://app.testsigma.com/api/v1/execution_results
 TESTSIGMA_JUNIT_REPORT_URL=https://app.testsigma.com/api/v1/reports/junit
-MAX_WAITTIME_EXCEEDED_ERRORMSG="Time waiting for test run completion exceeded specified Maximum Wait Time of $MAX_WAIT_TIME_FOR_SCRIPT_TO_EXIT seconds. 
-Please log-in to Testsigma to check Test Plan run status. 
-You can visit the URL specified in \"app_url\" JSON parameter in the response to go to the Test Plan results page directly. 
+MAX_WAITTIME_EXCEEDED_ERRORMSG="Given Maximum Wait Time of $MAX_WAIT_TIME_FOR_SCRIPT_TO_EXIT minutes exceeded waiting for the Test Run completion. 
+Please log-in to Testsigma to check Test Plan run results. You can visit the URL specified in \"app_url\" JSON parameter in the response to go to the Test Plan results page directly. 
 For example, \"app_url\":\"https://dev.testsigma.com/#/projects/31/applications/53/version/72/report/executions/197/runs/819/environments\""
 #**********************************
 
@@ -39,7 +37,7 @@ For example, \"app_url\":\"https://dev.testsigma.com/#/projects/31/applications/
 for i in "$@"
   do
   case $i in
-    -a=*|--apikey=*)
+    -k=*|--apikey=*)
     TESTSIGMA_API_KEY="${i#*=}"
     shift
     ;;
@@ -51,7 +49,7 @@ for i in "$@"
     MAX_WAIT_TIME_FOR_SCRIPT_TO_EXIT="${i#*=}"
     shift
     ;;
-   -r=*|--reportfilepath=*)
+    -r=*|--reportfilepath=*)
     JUNIT_REPORT_FILE_PATH="${i#*=}"
     shift
     ;;
@@ -64,15 +62,16 @@ for i in "$@"
     shift
     ;;
    -h|--help)
-    echo "Arguments \n[-a | --apikey]=<TESTSIGMA_API_KEY>"
-    echo "[-i | --testplanid]=<TESTSIGMA_TEST_PLAN_ID>"
-    echo "[-t | --maxtimeinmins=<MAX_WAIT_TIME_IN_MINS>"
-    echo "[-r | reportfilepath] =<JUNIT_REPORT_FILE_PATH>"
-    echo "[-d | runtimedata] =<OPTIONAL COMMA SEPARATED KEY VALUE PAIRS>"
-    echo "[-b | buildno] =<BUILD_NO_IF_ANY>"
+    echo "Arguments: "
+    echo " [-k | --apikey] = <TESTSIGMA_API_KEY>"
+    echo " [-i | --testplanid] = <TESTSIGMA_TEST_PLAN_ID>"
+    echo " [-t | --maxtimeinmins] = <MAX_WAIT_TIME_IN_MINS>"
+    echo " [-r | --reportfilepath] = <JUNIT_REPORT_FILE_PATH>"
+    echo " [-d | --runtimedata] = <OPTIONAL COMMA SEPARATED KEY VALUE PAIRS>"
+    echo " [-b | --buildno] = <BUILD_NO_IF_ANY>"
 
-    printf "Ex:\n bash testsigma_cicd.sh --apikey=YSWfniLEWYK7aLrS-FhYUD1kO0MQu9renQ0p-oyCXMlQ --testplanid=230 --maxtimeinmins=180 --reportfilepath=./junit-report.xml \n\n"
-    printf "Ex: With Runtimedata parameters\n bash testsigma_cicd.sh --apikey=YSWfniLEWYK7aLrS-FhYUD1kO0MQu9renQ0p-oyCXMlQ --testplanid=230 --maxtimeinmins=180
+    printf "Example:\n bash testsigma_cicd.sh --apikey=YSWfniLEWYK7aLrS-FhYUD1kO0MQu9renQ0p-oyCXMlQ --testplanid=230 --maxtimeinmins=180 --reportfilepath=./junit-report.xml \n\n"
+    printf "With Runtimedata parameters:\n bash testsigma_cicd.sh --apikey=YSWfniLEWYK7aLrS-FhYUD1kO0MQu9renQ0p-oyCXMlQ --testplanid=230 --maxtimeinmins=180
     --reportfilepath=./junit-report.xml --runtimedata=\"buildurl=http://test1.url.com,data1=testdata\" --buildno=773\n\n"
 
     shift
@@ -96,7 +95,7 @@ get_status(){
 
 function checkTestPlanRunStatus(){
   IS_TEST_RUN_COMPLETED=0
-  for ((i=0;i<=NO_OF_POLLS;i++))
+  for ((i=0;i<=POLL_COUNT;i++))
   do
     get_status
     if [ $EXECUTION_STATUS = "STATUS_IN_PROGRESS" ]; then
@@ -241,7 +240,7 @@ if [ ! $HTTP_STATUS -eq 200  ]; then
   EXITCODE=1
   #Exit with a failure.
 else
-  echo "Number of maximum polls to be done: $NO_OF_POLLS"
+  echo "Number of maximum polls to be done: $POLL_COUNT"
   checkTestPlanRunStatus
   saveFinalResponseToJUnitFile
   saveFinalResponseToJSONFile
